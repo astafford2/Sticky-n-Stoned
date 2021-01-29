b83a5d3f6a08b57de7a52c607be27f411bec7751
Andrew Stafford <astafford2@bsu.edu>
2021-01-28 21:31:32 -0500

    Add else's to stop player once keys let up

--- project/src/characters/Player.gd
+++ project/src/characters/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 
-const run_speed := 200
+const run_speed := 100
 
 var velocity := Vector2()
 
@@ -17,11 +17,15 @@ func _physics_process(delta):
 		velocity.y = -run_speed
 	elif Input.is_action_pressed("move_down"):
 		velocity.y = run_speed
+	else:
+		velocity.y = 0
 	if Input.is_action_pressed("move_right"):
 		player_sprite.flip_h = false
 		velocity.x = run_speed
 	elif Input.is_action_pressed("move_left"):
 		player_sprite.flip_h = true
 		velocity.x = -run_speed
+	else:
+		velocity.x = 0
 	
 	velocity = move_and_slide(velocity, Vector2.ZERO)
