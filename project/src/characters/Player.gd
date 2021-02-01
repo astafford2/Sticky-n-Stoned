f79bf0303044570573134c2b0afa07635b77d09e
Christo <c.stasz@outlook.com>
2021-01-31 19:07:57 -0500

    added basic shooting and glue functionality

--- project/src/characters/Player.gd
+++ project/src/characters/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+export (PackedScene) var GlueBullet
 
 const run_speed := 100
 
@@ -12,7 +13,8 @@ func _ready():
 	pass
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	$Muzzel.look_at(get_global_mouse_position())
 	if Input.is_action_pressed("move_up"):
 		velocity.y = -run_speed
 	elif Input.is_action_pressed("move_down"):
@@ -27,8 +29,14 @@ func _physics_process(delta):
 		velocity.x = -run_speed
 	else:
 		velocity.x = 0
-	
+	if Input.is_action_just_pressed("shoot_glue"):
+		shoot()
 	player_sprite.animation = "run" if velocity != Vector2.ZERO else "idle"
 	
 	player_sprite.play()
 	velocity = move_and_slide(velocity, Vector2.ZERO)
+
+func shoot():
+	var b = GlueBullet.instance()
+	owner.add_child(b)
+	b.transform = $Muzzel.global_transform
---
+++ project/src/projectiles/GlueBullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+export var speed = 200
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	position += transform.x *speed * delta
+
+func _on_GlueBullet_body_entered(body):
+	if !body.has_method("shoot"):
+		queue_free()
+	if body.is_in_group("glueable"):
+		body.glue(45, 5)
+		queue_free()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
