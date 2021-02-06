77f9e634fb3392956714e072808e008ce7254535
Andrew Stafford <astafford2@bsu.edu>
2021-02-06 17:32:35 -0500

    Remove unneccesary code from pitfall script

--- project/src/Pitfall.gd
+++ project/src/Pitfall.gd
@@ -1,13 +1,6 @@
 extends Area2D
 
 
-var delta2
-
-
-func _process(delta):
-	delta2 = delta
-
-
 func _on_Pitfall_body_entered(body):
 	if body.has_method("shoot"):
 		body.pitfalled(position+Vector2(16, 16))
