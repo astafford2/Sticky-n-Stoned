af0b977754a4838a50445b9e09c0654104e1bd96
Andrew Stafford <astafford2@bsu.edu>
2021-02-09 20:26:41 -0500

    Add new script for smaller chair pieces to give them health

---
+++ project/src/projectiles/ChairPiece.gd
@@ -0,0 +1,56 @@
+extends "res://src/projectiles/Projectile.gd"
+
+var health := 5
+
+onready var interactionBox := $InteractionBox
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.add_to_group("inventoryItem")
+
+func hitActivity(delta):
+	position -= transform.x * speed * delta / 2
+	sprite.rotation -= 10 * delta
+
+func projectileActivity(delta):
+	position += transform.x * speed * delta
+	sprite.rotation += 30 * delta
+
+func _process(_delta):
+	if health <= 0:
+		queue_free()
+
+func Interact(body):
+	thrower = body
+	self.get_parent().remove_child(self)
+	body.add_child(self)
+	position =  Vector2(0, 20)
+	interactionBox.set_deferred("disabled", true)
+
+func Use():
+	remove_from_group("interactable")
+	projectile = true
+	var player = self.get_parent()
+	player.remove_child(self)
+	player.get_parent().add_child(self)
+	position = player.get_position()
+	hurtBox.set_deferred("disabled", false)
+	return true #tells the player that the object is no longer in their inventory
+
+func HitsAndFalls():
+	health -=1
+	add_to_group("interactable")
+	projectile = false
+	hit = true
+	thrower = null
+	hurtBox.set_deferred("disabled", true)
+	yield(get_tree().create_timer(0.5), "timeout")
+	interactionBox.set_deferred("disabled", false)
+	hit = false
+
+func _on_body_entered(body):
+	if projectile and !body.has_method("shoot"):
+		if body.is_in_group("enemies"):
+			body.enemy_hit(1, thrower)
+		HitsAndFalls()
