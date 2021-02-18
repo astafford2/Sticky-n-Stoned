a011efb9ff763ea394facc19c91872e83d93c0a5
Andrew Stafford <astafford2@bsu.edu>
2021-02-18 11:54:24 -0500

    Reverse enemy AI so shooter runs from player

--- project/src/characters/DemonShaman.gd
+++ project/src/characters/DemonShaman.gd
@@ -11,4 +11,50 @@ onready var glue_landing_fx := $GlueLanding
 
 
 func _ready():
-	pass
+	RUN_SPEED = 120
+	Health = 4
+
+
+func _process(_delta):
+	if Health <= 0:
+		kill_enemy()
+
+
+func _physics_process(_delta):
+	velocity = Vector2.ZERO
+	if Target:
+		velocity = -(position.direction_to(Target.position) * RUN_SPEED)
+	velocity = move_and_slide(velocity, Vector2.ZERO)
+
+func glue(amount, time):
+	if !glued:
+		glued = true
+		glue_landing_fx.play()
+		spatter = GlueSpatter.instance()
+		self.call_deferred("add_child", spatter)
+		spatter.position += Vector2(0, 20)
+		RUN_SPEED = RUN_SPEED-amount
+		yield(get_tree().create_timer(time), "timeout")
+		spatter.queue_free()
+		RUN_SPEED = RUN_SPEED+amount
+		glued=false
+
+
+func damagedActivity(thrower, damage):
+	Health -= damage
+	Target = thrower
+
+
+func kill_enemy():
+	call_deferred("queue_free")
+	if spatter:
+		spatter.call_deferred("queue_free")
+
+
+func _on_DetectRadius_body_entered(body):
+	if body.has_method("shoot"):
+		Target = body
+
+
+func _on_DetectRadius_body_exited(_body):
+	Target = null
