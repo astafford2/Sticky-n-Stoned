deac359d71e8cb2564e5a664f897591112af085b
Andrew Stafford <astafford2@bsu.edu>
2021-02-25 14:26:13 -0500

    Set basic textures in PitfallTrap scene

--- project/src/tiles/PitfallTrap.gd
+++ project/src/tiles/PitfallTrap.gd
@@ -4,13 +4,18 @@ extends Traps
 var rect : Rect2
 
 onready var pit := $PitfallShape
+onready var open := $Open
+onready var closed := $Closed
 
 
 func _ready():
 	rect = Rect2(pit.global_position - pit.shape.extents, pit.shape.extents * 2)
+	activated = true
 
 
 func activate(_delta):
+	closed.visible = false
+	open.visible = true
 	var intersecting = get_overlapping_areas()
 	if intersecting.size() > 0:
 		for area in get_overlapping_areas():
@@ -18,4 +23,5 @@ func activate(_delta):
 
 # what the child trap will do when deactivated or idle
 func deactivated(_delta):
-	pass
+	closed.visible = true
+	open.visible = false
