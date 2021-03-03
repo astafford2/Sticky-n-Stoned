8cd7a4e08861c6a4cf87e25bea55121e186588fe
Andrew Stafford <astafford2@bsu.edu>
2021-03-02 23:56:39 -0500

    Create navigation2d polygon for enemy and draw closest path to player

---
+++ project/src/NavigationTesting.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+onready var player := $Player
+onready var enemy := $BigDemon
+onready var nav := $Navigation2D
+onready var path_line := $Line2D
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	var path = nav.get_simple_path(enemy.position, player.position)
+	path_line.points = path
