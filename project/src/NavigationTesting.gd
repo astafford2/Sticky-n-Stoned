3c703173112591a952ca6c5cb33888ae85f6536f
Andrew Stafford <astafford2@bsu.edu>
2021-03-09 11:18:17 -0500

    Setup math to get direct opposite path from shaman to player

--- project/src/NavigationTesting.gd
+++ project/src/NavigationTesting.gd
@@ -8,8 +8,14 @@ onready var player := $Player
 onready var nav_instance := $NavigationPolygonInstance
 onready var enemy := $BigDemon
 onready var shaman := $DemonShaman
+onready var nav := $Navigation2D
+onready var line := $Line2D
 
 
 func _process(_delta):
-	for enemy in get_tree().get_nodes_in_group("enemies"):
-		enemy.set_navigation(nav_instance.navpoly, player)
+#	for enemy in get_tree().get_nodes_in_group("enemies"):
+#		enemy.set_navigation(nav_instance.navpoly, player)
+	var shaman_to_player = Vector2(player.position.x - shaman.position.x, player.position.y - shaman.position.y)
+	var shaman_runto = shaman.position - shaman_to_player
+	var path = nav.get_simple_path(shaman.position, shaman_runto)
+	line.points = path
