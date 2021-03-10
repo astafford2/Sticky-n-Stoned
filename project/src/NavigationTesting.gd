45a0c16adc4f6fd0b6861c83fb46dded722dfec0
Andrew Stafford <astafford2@bsu.edu>
2021-03-10 16:32:07 -0500

    Work towards merging trapdoor navpoly with room poly

--- project/src/NavigationTesting.gd
+++ project/src/NavigationTesting.gd
@@ -6,12 +6,16 @@ var shaman_runto := Vector2()
 
 onready var player := $Player
 onready var nav_instance := $NavigationPolygonInstance
-onready var enemy := $BigDemon
+onready var trapdoor := $Trapdoor
 onready var shaman := $DemonShaman
 onready var path := $Line2D
 
 
 func _ready():
+	var multi_poly = Geometry.merge_polygons_2d(nav_instance.navpoly.get_vertices(), trapdoor.navpoly.get_vertices())
+	nav_instance.navpoly.clear_polygons()
+	nav_instance.navpoly.clear_outlines()
+	nav_instance.navpoly.add_polygon(multi_poly)
 	for enemy in get_tree().get_nodes_in_group("enemies"):
 		enemy.set_navigation(nav_instance.navpoly, player)
 
