0499f1b0d4bd369dd557854612e71b2c25b2d966
Andrew Stafford <astafford2@bsu.edu>
2021-03-20 16:59:43 -0400

    Set slime flesh to target the king slime

--- project/src/characters/Level1/SlimeFlesh.gd
+++ project/src/characters/Level1/SlimeFlesh.gd
@@ -1,17 +1,22 @@
 extends GreenSlime
 
 
+var slime_flesh_target : AnimatedSprite = null
+
+
 func _ready():
 	RUN_SPEED = 105
 	Health = 1
 # warning-ignore:return_value_discarded
 	SignalMaster.connect("overlapped", self, "_on_feet_overlapped")
+	
+	slime_flesh_target = get_parent().get_node("KingSlimeProp")
 
 
 func _physics_process(_delta):
 	velocity = Vector2.ZERO
-	if Target:
-		velocity = global_position.direction_to(Target.global_position) * RUN_SPEED
+	if slime_flesh_target:
+		velocity = global_position.direction_to(slime_flesh_target.global_position) * RUN_SPEED
 	velocity = move_and_slide(velocity, Vector2.ZERO)
 
 
