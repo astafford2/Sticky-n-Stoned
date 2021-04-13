c49a00b3ddf73bf714bb7228030c1bfbb07ec87d
Andrew Stafford <astafford2@bsu.edu>
2021-04-13 00:25:17 -0400

    Set new bus volumes based on pause sliders

--- project/src/GUI/PauseMenu.gd
+++ project/src/GUI/PauseMenu.gd
@@ -1,9 +1,38 @@
 extends PopupPanel
 
 
+onready var master_slider := $MarginContainer/VBoxContainer/MasterVolume/MasterSlider
+onready var music_slider := $MarginContainer/VBoxContainer/MusicVolume/MusicSlider
+onready var sound_slider := $MarginContainer/VBoxContainer/SoundVolume/SoundSlider
+
+
 func _ready():
 	pass
 
 
 func _on_Unpause_pressed():
 	get_parent().unpause()
+
+
+func _on_MasterSlider_value_changed(value):
+	if value == -24:
+		AudioServer.set_bus_mute(0, true)
+	else:
+		AudioServer.set_bus_mute(0, false)
+		AudioServer.set_bus_volume_db(0, master_slider.value)
+
+
+func _on_MusicSlider_value_changed(value):
+	if value == -24:
+		AudioServer.set_bus_mute(1, true)
+	else:
+		AudioServer.set_bus_mute(1, false)
+		AudioServer.set_bus_volume_db(1, music_slider.value)
+
+
+func _on_SoundSlider_value_changed(value):
+	if value == -24:
+		AudioServer.set_bus_mute(2, true)
+	else:
+		AudioServer.set_bus_mute(2, false)
+		AudioServer.set_bus_volume_db(2, sound_slider.value)
